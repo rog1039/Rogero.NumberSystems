c68845a8ad74148ea86f5bfa9558129c9a2579b8
Paul Rogero <progero@gmail.com>
2015-08-26 18:00:35 -0400

    Added missing file.

---
+++ src/Rogero.NumberSystems/Rogero.NumberSystems/Number.cs
@@ -0,0 +1,19 @@
+﻿namespace Rogero.NumberSystems
+{
+    public class Number
+    {
+        public NumberSystem NumberSystem { get; set; }
+        public string Value { get; set; }
+
+        public Number(NumberSystem numberSystem, string value)
+        {
+            NumberSystem = numberSystem;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value,3} [{NumberSystem}]";
+        }
+    }
+}
